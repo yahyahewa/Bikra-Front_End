24018571b3370400ce6f59024bc721c7ebaf2d40
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-03-24 04:44:39 +0300

    configuer Router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 // import css file
 import "./App.css";
+import MainNavbar from "./components/MainNavbar";
+//--//-----------//--//
+// import components
+import MainNavbar from "./components/MainNavbar";
+//--//-----------//--//
+// import packeg
+import { Route, Routes } from "react-router-dom";
 //--//-----------//--//
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<MainNavbar />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
