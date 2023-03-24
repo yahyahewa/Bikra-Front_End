6c60fd407ff5fbe25dd99adb3ee02f9182fa0131
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-03-25 00:20:58 +0300

    add big ston colors for tailwind faile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "big-stone": {
+          50: "#f3f7fc",
+          100: "#e6eff8",
+          200: "#c7ddf0",
+          300: "#95c1e4",
+          400: "#5c9fd4",
+          500: "#3783c0",
+          600: "#2768a2",
+          700: "#215383",
+          800: "#1f486d",
+          900: "#112233",
+        },
+      },
+    },
   },
   plugins: [],
 };
