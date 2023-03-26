6f1655199c1a4ad9bc8f530430d68fa48d4499b0
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-03-26 03:20:33 +0300

    update 1 shop slide

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import MainNavbar from "./MainNavbar";
+import ShopSlide from "./ShopSlide";
+function Home() {
+  return (
+    <div>
+      <MainNavbar />
+      <ShopSlide />
+    </div>
+  );
+}
+
+export default Home;
--- src/components/ShopSlide.jsx
+++ src/components/ShopSlide.jsx
@@ -1,79 +1,210 @@
 import React, { useState, useEffect } from "react";
-// import img from "../assets/images/cart.png";
 import "./components.css";
 function ShopSlide() {
-  const slides = [
+  const pathImg =
+    "https://media.thebodyshop.com/i/thebodyshop/Bond-Street-Station-1272x1272?$amplience-ct1-xs-img1$&wid=1900&hei=900&zoom=0.25&pcrop={($this.metadata.pointOfInterest.x*100%)-((($wid/$zoom)/2)-(($this.metadata.pointOfInterest.w/2)*100%))},{($this.metadata.pointOfInterest.y*100%)-((($hei/$zoom)/2)-(($this.metadata.pointOfInterest.h/2)*100%))},{$wid/$zoom},{$hei/$zoom}&w=$wid&h=$hei&sm=c&fmt=auto";
+  const items = [
     {
-      image:
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
-      title: "title",
-      description: "kurdstan shop",
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
     },
     {
-      image:
-        "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
-      title: "title",
-      description: "kurdstan shop",
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
+    },
+    {
+      image: pathImg,
+      name: "kurdstan market erbil",
+      price: "1000$",
     },
   ];
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const [isHovering, setIsHovering] = useState(false);
+
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [isTimerPaused, setIsTimerPaused] = useState(false);
+  const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (!isHovering) {
-        setCurrentSlideIndex((prevIndex) =>
-          prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+      if (!isTimerPaused) {
+        setSlideIndex(
+          (prevIndex) =>
+            (prevIndex + 1) % Math.ceil(items.length / getItemsPerSlide())
         );
       }
-    }, 5000);
+    }, 3000);
     return () => clearInterval(timer);
-  }, [slides.length, isHovering]);
+  }, [isTimerPaused]);
+
+  const handlePrevSlideClick = () => {
+    setSlideIndex((prevIndex) =>
+      prevIndex === 0
+        ? Math.ceil(items.length / getItemsPerSlide()) - 1
+        : prevIndex - 1
+    );
+    setIsDark(!isDark);
+  };
+
+  const handleNextSlideClick = () => {
+    setSlideIndex(
+      (prevIndex) =>
+        (prevIndex + 1) % Math.ceil(items.length / getItemsPerSlide())
+    );
+  };
+
+  const handleMouseEnter = () => {
+    setIsTimerPaused(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsTimerPaused(false);
+  };
+
+  const getItemsPerSlide = () => {
+    if (window.innerWidth >= 1200) {
+      return 7;
+    } else if (window.innerWidth >= 768) {
+      return 5;
+    } else {
+      return 3;
+    }
+  };
+
+  const renderItems = () => {
+    const itemsPerSlide = getItemsPerSlide();
+    const startIndex = slideIndex * itemsPerSlide;
+    const endIndex = startIndex + itemsPerSlide;
+    const slideItems = items.slice(startIndex, endIndex);
+
+    return slideItems.map((item, index) => (
+      <a href="#" key={index}>
+        <div
+          key={index}
+          className={`overflow-hidden object-contain
+         rounded w-[120px] lg:w-[150px] h-[130px]   ${
+           isDark
+             ? `bg-big-stone-800 text-big-stone-100`
+             : `text-big-stone-100 bg-big-stone-900`
+         }`}
+        >
+          <img src={item.image} className="w-full h-[95px]  " />
+          <h1
+            className={` text-center 
+            capitalize underline mt-1 text-sm lg:text-base`}
+          >
+            {item.name}
+          </h1>
+        </div>
+      </a>
+    ));
+  };
 
   return (
-    <section
-      className={`relative w-full h-48`}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
+    <div
+      className={`relative w-full md:w-[90%] max-w-[1200px] m-auto mt-3  
+       ${
+         isDark ? `bg-big-stone-900 text-big-stone-100` : `text-big-stone-900`
+       }`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <div className="relative w-full h-full overflow-hidden">
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className={`slide-cart-slide absolute top-0 left-0 w-full h-full opacity-0 transition ${
-              index === currentSlideIndex ? "opacity-100" : ""
-            }`}
-          >
-            <img src={slide.image} alt={slide.title} />
-            <h2>{slide.title}</h2>
-            <p>{slide.description}</p>
-            <img className="ml-11" src={slide.image} alt={slide.title} />
-            <h2>{slide.title}</h2>
-            <p>{slide.description}</p>
-          </div>
-        ))}
+      <h1>prime shop</h1>
+      <div className="flex flex-wrap justify-evenly items-center ">
+        {renderItems()}
       </div>
-      <button
-        className="slide-cart-button"
-        onClick={() =>
-          setCurrentSlideIndex((prevIndex) =>
-            prevIndex === slides.length - 1 ? 0 : prevIndex + 1
-          )
-        }
-      >
-        Next
+      <button className="prev" onClick={handlePrevSlideClick}>
+        &#10094;
       </button>
-      <button
-        className="slide-cart-button left-1 w-3"
-        onClick={() =>
-          setCurrentSlideIndex((prevIndex) =>
-            prevIndex === 0 ? slides.length - 1 : prevIndex - 1
-          )
-        }
-      >
-        preve
+      <button className="next" onClick={handleNextSlideClick}>
+        &#10095;
       </button>
-    </section>
+    </div>
   );
 }
 
