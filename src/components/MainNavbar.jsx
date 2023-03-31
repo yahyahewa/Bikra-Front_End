39347508dc2f591acf1d47d6ce45aea0578bd25c
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-03-31 18:15:11 +0300

    update 2 navbar

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,113 +1,102 @@
 import React, { useState } from "react";
-import "./components.css";
+// import "./components.css";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import logo from "../assets/images/company.png";
-import cart from "../assets/images/cart.png";
+import cart from "../assets/images/cart-bascket.svg";
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
-  const [lang, setLang] = useState(false);
+  const isDark = false;
+  const [menubar, setMenuBar] = useState(false);
   function handleClick() {
-    if (!click) setClick(true);
-    else setClick(false);
+    if (!menubar) setMenuBar(true);
+    else setMenuBar(false);
   }
-  const [selectedOption, setSelectedOption] = useState("");
 
   const handleChange = (event) => {
     setSelectedOption(event.target.value);
   };
   return (
-    <nav className="flex justify-start w-full bg-big-stone-900 py-1">
-      <div className="flex items-center justify-between w-full  bg-big-stone-900">
-        {/* box search */}
+    <nav className=" max-w-[1500px] w-full m-auto bg-big-stone-900 py-1">
+      <section className="flex justify-center gap-x-1 w-full">
+        <Link to="/">
+          <img src={logo} className={`w-[40px]`} />
+        </Link>
+        {/* start search bar */}
         <div
-          className={`rounded  w-full md:w-[50%] flex 
-          justify-between overflow-hidden 
-           bg-big-stone-900 m-aut text-xs md:text-base`}
+          className={`rounded bg-[#fff] flex overflow-hidden w-[75%] sm:w-[60%] md:w-[50%] lg:w-[40%]`}
         >
-          <Link to="/">
-            <img className={`w-10`} src={logo} />
-          </Link>
-          <select className={`w-auto outline-none text-center rounded-l `}>
+          <select className={`outline-none w-fit text-center rounded-l `}>
             <option>All</option>
             <option>Computer</option>
             <option>headfont</option>
           </select>
           <input
-            type="search"
-            className={`w-[90%] border-l outline-none pl-1`}
-            placeholder={`Search Bikra...`}
+            className={`w-full border-l pl-2 outline-none `}
+            type={`search`}
+            placeholder="Search Bikra ..."
           />
           <i
             className="fa-solid fa-magnifying-glass flex items-center
-           scale-110 hover:scale-125 transition bg-big-stone-500 px-1 text-big-stone-50"
+           scale-110 hover:scale-125 transition px-1 text-big-stone-900 mr-1"
           ></i>
         </div>
-        {/* box responsive */}
-        <div
-          className={`${
-            click
-              ? `nav-options flex active`
-              : `nav-options flex justify-start w-[48%]`
-          }`}
+        {/* end search bar */}
+        {/* strat menu button */}
+        <button
+          className=" text-big-stone-200 overflow-hidden"
+          onClick={() => {
+            handleClick();
+          }}
         >
-          <Button
-            class={`mr-2 text-big-stone-50 border-0 md:border rounded px-2
-             py-1 font-normal md:font-medium capitalize  transition`}
-            text={"filter"}
-            type="button"
-          />
-          <Button
-            type="button"
-            class={`bg-big-stone-500 py-1 px-2 rounded h-full ml-1
-             text-big-stone-50 hover:bg-big-stone-600 transition`}
-            text="Create a Website"
-          />
-          <Button
-            type="button"
-            class={` underline py-1 px-2 rounded h-full ml-1 text-big-stone-50`}
-            text="Login"
-          />
-          <button clas={`ml-1`}>
-            <img
-              src={cart}
-              className={`w-[20px] sm:w-[22px] hover:scale-110 m-auto transition`}
-            />
-          </button>
-          <select
-            className={`ml-2 bg-transparent text-big-stone-50 rounded
-             py-1 font-normal md:font-medium capitalize w-fit ltr outline-none`}
-            value={selectedOption}
-            onChange={handleChange}
-          >
-            <option className="text-big-stone-900 w-fit ltr" value="option1">
-              English
-            </option>
-            <option className="text-big-stone-900 w-fit ltr" value="option2">
-              کوردی
-            </option>
-            <option className="text-big-stone-900 w-fit ltr" value="option3">
-              عربی
-            </option>
-          </select>
-          <button
-            className={` text-sm lg:text-base text-big-stone-50 rounded px-2
-             py-1 font-normal md:font-medium capitalize `}
-          >
-            <i className="fa-solid fa-lightbulb"></i> Dark
-          </button>
-        </div>
-      </div>
-
-      <button
-        className="inline-block md:hidden text-big-stone-200 px-1"
-        onClick={() => {
-          handleClick();
-        }}
+          <span
+            className={`bg-big-stone-100 h-[6px] w-8 mt-1 mb-[6px] block rounded-full transition-all  
+        ${menubar ? `rotate-[45deg]` : ``}`}
+          ></span>
+          <span
+            className={`bg-big-stone-100 h-[6px] w-8 mt-1 mb-[6px] block roun transition-all rounded-full 
+        ${menubar ? `hidden` : ``}`}
+          ></span>
+          <span
+            className={`bg-big-stone-100 h-[6px] w-8 mt-1 mb-[6px] block rounded-full  transition-all 
+        ${menubar ? `rotate-[-45deg]` : ``}`}
+          ></span>
+        </button>
+        {/* strat menu button */}
+      </section>
+      <article
+        className={`w-full flex flex-col items-center gap-y-1 pt-2 transition-all overflow-hidden 
+        ${menubar ? `h-auto` : `h-0`}`}
       >
-        menu
-      </button>
+        <Button
+          type="button"
+          class={`bg-big-stone-200 text-big-stone-900 w-[75%] sm:w-[60%] md:w-[50%] lg:w-[40%] rounded
+          p-2 font-bold hover:bg-big-stone-300 transition md:text-lg md:p-1 md:py-2 ml-[6px]`}
+          text="Create a Free Website"
+        />
+        <Button
+          type="button"
+          class={`text-big-stone-100 underline w-fit  rounded
+          py-1 px-2 font-bold capitalize`}
+          text="login"
+        />
+        <i class="fa-solid fa-cart-shopping fa-xl m-3 text-big-stone-100"></i>
+        <select
+          className={`ml-2 bg-transparent text-big-stone-50 rounded
+             py-1 font-normal md:font-medium capitalize w-fit ltr outline-none`}
+          onChange={handleChange}
+        >
+          <option className="text-big-stone-900 w-fit ltr" value="option1">
+            English
+          </option>
+          <option className="text-big-stone-900 w-fit ltr" value="option2">
+            کوردی
+          </option>
+          <option className="text-big-stone-900 w-fit ltr" value="option3">
+            عربی
+          </option>
+        </select>
+      </article>
     </nav>
   );
 };
