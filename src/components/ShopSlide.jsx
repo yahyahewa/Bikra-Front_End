b19c1e1c66597573056aca7eca632fbb7d4449c9
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-04-07 00:37:01 +0300

    make responsive to card in shop slide

--- src/components/ShopSlide.jsx
+++ src/components/ShopSlide.jsx
@@ -1,107 +1,112 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./components.css";
 function ShopSlide() {
   const pathImg =
     "https://media.thebodyshop.com/i/thebodyshop/Bond-Street-Station-1272x1272?$amplience-ct1-xs-img1$&wid=1900&hei=900&zoom=0.25&pcrop={($this.metadata.pointOfInterest.x*100%)-((($wid/$zoom)/2)-(($this.metadata.pointOfInterest.w/2)*100%))},{($this.metadata.pointOfInterest.y*100%)-((($hei/$zoom)/2)-(($this.metadata.pointOfInterest.h/2)*100%))},{$wid/$zoom},{$hei/$zoom}&w=$wid&h=$hei&sm=c&fmt=auto";
   const items = [
     {
-      image: pathImg,
-      name: "kurdstan market erbil",
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
+      price: "1000$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+      name: "siver fashion collection",
       price: "1000$",
     },
     {
       image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
-      price: "1000$",
-    },
-    {
-      image: pathImg,
-      name: "kurdstan market erbil",
+      name: "siver fashion collection",
       price: "1000$",
     },
   ];
@@ -118,7 +123,7 @@ function ShopSlide() {
             (prevIndex + 1) % Math.ceil(items.length / getItemsPerSlide())
         );
       }
-    }, 3000);
+    }, 4000);
     return () => clearInterval(timer);
   }, [isTimerPaused]);
 
@@ -148,11 +153,11 @@ function ShopSlide() {
 
   const getItemsPerSlide = () => {
     if (window.innerWidth >= 1200) {
-      return 7;
-    } else if (window.innerWidth >= 768) {
       return 5;
+    } else if (window.innerWidth >= 768) {
+      return 4;
     } else {
-      return 3;
+      return 2;
     }
   };
 
@@ -163,47 +168,51 @@ function ShopSlide() {
     const slideItems = items.slice(startIndex, endIndex);
 
     return slideItems.map((item, index) => (
-      <a href="#" key={index}>
+      <Link to="#" key={index}>
         <div
           key={index}
-          className={`overflow-hidden object-contain
-         rounded w-[120px] lg:w-[150px] h-[130px]   ${
-           isDark
-             ? `bg-big-stone-800 text-big-stone-100`
-             : `text-big-stone-100 bg-big-stone-900`
-         }`}
+          className={`overflow-hidden
+          w-[120px] h-[170px]  p-1 `}
         >
-          <img src={item.image} className="w-full h-[95px]  " />
-          <h1
-            className={` text-center 
-            capitalize underline mt-1 text-sm lg:text-base`}
+          <img
+            src={item.image}
+            className={`w-[110px]
+             shadow-lg h-[110px] m-auto overflow-hidden rounded-full object-cover hover:scale-105 hover:shadow-2xl hover:shadow-yellow-200 ease-in-out duration-300 `}
+          />
+          <p
+            className={`text-sm text-slate-800 m-[2px] text-center font-medium`}
           >
             {item.name}
-          </h1>
+          </p>
         </div>
-      </a>
+      </Link>
     ));
   };
 
   return (
-    <div
-      className={`relative w-full md:w-[90%] max-w-[1200px] m-auto mt-3  
-       ${
-         isDark ? `bg-big-stone-900 text-big-stone-100` : `text-big-stone-900`
-       }`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <h1>prime shop</h1>
-      <div className="flex flex-wrap justify-evenly items-center ">
-        {renderItems()}
+    <div className="w-full relative">
+      <div className={` bg-blue-200  h-[113px] absolute w-full`}></div>
+      <div
+        className={`relative w-full md:w-[90%] max-w-[1500px] m-auto mt-3 z-10 `}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <a
+          className={`ml-2 text-center w-full md:ml-0 mb-2 font-medium text-slate-900 
+          text-lg capitalize inline-block`}
+        >
+          prime shop
+        </a>
+        <div className="flex flex-wrap justify-evenly items-center ">
+          {renderItems()}
+        </div>
+        <button className="prev" onClick={handlePrevSlideClick}>
+          &#10094;
+        </button>
+        <button className="next" onClick={handleNextSlideClick}>
+          &#10095;
+        </button>
       </div>
-      <button className="prev" onClick={handlePrevSlideClick}>
-        &#10094;
-      </button>
-      <button className="next" onClick={handleNextSlideClick}>
-        &#10095;
-      </button>
     </div>
   );
 }
