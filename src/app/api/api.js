1a1fb000063d7a2a81107f2112cf1f2f01b52cc5
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-04-16 11:58:57 +0300

    update baseurl and update apiurl2

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://localhost:3000/",
-    baseUrl: "data/db.json",
+    baseUrl: "http://localhost:3000/",
+    // baseUrl: "data/db.json",
   }),
   tagTypes: ["non_refresh"],
   endpoints: () => ({}),
