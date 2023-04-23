3ac703f1a3a2fb560385fffd7da8f83e63dc4ef9
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-04-23 18:39:46 +0300

    edite some route and add some routes (signup, login)

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import "./App.css";
 import Home from "./components/Home.jsx";
 import ItemPage from "./components/ItemPage";
 import SingleItem from "./components/SingleItem";
-import AddtoCart from "./components/Cart";
+import AddtoCart from "./components/CartsPage";
 import Login from "./components/Login";
 import CreateWebsite from "./components/CreateWebsite";
+import ItemEditor from "./components/account/ProductEditor";
+import Account from "./components/account/Account";
 //--//-----------//--//
 function App() {
   return (
@@ -18,12 +20,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/main#CreateWebs" element={<Home />} />
         <Route path="/singleitem/:id" element={<SingleItem />}></Route>
+        <Route path="/itemeditor/:id" element={<ItemEditor />}></Route>
         <Route path="/item" element={<ItemPage />}></Route>
+        <Route path="/account" element={<Account />}></Route>
         <Route path="/signup" element={<CreateWebsite />}></Route>
         <Route path="/item/:categorey" element={<ItemPage />}></Route>
         <Route path="/cart/" element={<AddtoCart />}></Route>
         <Route path="/item/:categorey" element={<ItemPage />}></Route>
-        <Route path="/login/:username" element={<Login />}></Route>
+        <Route path="/login/" element={<Login />}></Route>
       </Routes>
     </div>
   );
