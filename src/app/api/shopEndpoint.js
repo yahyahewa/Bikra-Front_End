335d68c954ef2c2d2bcdd23d09d0008d45659f56
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-04-23 19:31:07 +0300

    add shopEndpoint for create all endpoint for accounts

---
+++ src/app/api/shopEndpoint.js
@@ -0,0 +1,27 @@
+import { api } from "./api";
+
+const website = api.injectEndpoints({
+  endpoints: (builder) => ({
+    // retrive seller acount information
+    //like login ,username , configration and product ......
+    retriveAccountInfo: builder.query({
+      query: () => `websites`,
+      providesTags: ["non_refresh"],
+    }),
+    // create account , add item and another
+    createAccount: builder.mutation({
+      query: (collection, data) => ({
+        url: `${collection}`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["non_refresh"],
+    }),
+  }),
+});
+export const { useRetriveAccountInfoQuery, useCreateAccountMutation } = website;
+
+// getWebUsername: builder.query({
+//   query: (username) => `websites?username=${username}`,
+//   providesTags: ["non_refresh"],
+// }),
