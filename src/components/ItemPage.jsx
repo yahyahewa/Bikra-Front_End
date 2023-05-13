8cbd69a2efd58e89d50e5f30ea2fa4213ba044e9
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-13 21:29:57 +0300

    connected with server and retriv data from database

--- src/components/ItemPage.jsx
+++ src/components/ItemPage.jsx
@@ -1,62 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "./Filter";
 import Navbar from "./MainNavbar";
 import "./components.css";
 import Loding from "./Loading";
 import { Link, useParams } from "react-router-dom";
-// import {
-//   useAddToCartMutation,
-//   useGetItemsQuery,
-//   useGetorderQuery,
-// } from "../app/api/itemEndpoints_";
 import Footer from "./Footer";
-import { useSelector } from "react-redux";
-import Li_nk from "./Li_nk";
-function ItemPage() {
-  const custID = useSelector((state) => state.user.custUserName);
-  // const {
-  //   data: order,
-  //   isLoading: orderLoding,
-  //   isError: orderError,
-  // } = useGetorderQuery();
-  const order = [];
-  const orderLoding = false;
-  const orderError = false;
-  // const [addToCart] = useAddToCartMutation();
-  function addCardHandle(id) {
-    let CanAdd = true;
-    orderError ? (
-      ``
-    ) : orderLoding ? (
-      <Loading />
-    ) : (
-      order.map((value) => {
-        if (value.itemid == id && value.status == "card") {
-          CanAdd = false;
-        }
-      })
-    );
-    let maxNumber;
-    if (CanAdd === true) {
-      let idArray = [0];
-      order.map((value) => {
-        idArray.push(value.id);
-      });
-      maxNumber = Math.max(...idArray);
-      window.localStorage.setItem("card", maxNumber + 1);
-      addToCart({
-        status: "card",
-        itemid: id,
-        custId: custID,
-        id: order.lenght,
-      });
-    }
+import Pagination from "./Pagination";
+import { userData } from "../Slice/userSlice";
+import { useGetSomeItemForHomeQuery } from "../app/api/productEndPoint";
+import { useAddToCartMutation } from "../app/api/orderEndpoint";
+import { useGetUserInformationQuery } from "../app/api/LoginAndSignUpEndPopiant";
+import { useDispatch, useSelector } from "react-redux";
+
+function ItemPage({ scrol }) {
+  const custID = useSelector((state) => state.user?.user?._id);
+  if (scrol) {
+    window.scrollTo({ top: 0 });
   }
   const { categorey } = useParams();
-  //const { data, isLoading, isError } = useGetItemsQuery();
-  const data = [];
-  const isLoading = false;
-  const isError = false;
+  const [addToCart] = useAddToCartMutation();
+  const dispatch = useDispatch();
+  const [filterAndPagination, setFilterAndPagination] = useState({
+    page: 1,
+    limit: 12,
+    search: "",
+    category: categorey,
+  });
+  const {
+    data: userInformation,
+    isError: isUSerError,
+    isLoading: isUserLoading,
+  } = useGetUserInformationQuery();
+  console.log();
+  useEffect(() => {
+    if (!isUSerError && !isUserLoading) {
+      dispatch(userData(userInformation?.data));
+    }
+  }, [userInformation]);
+  const { data, isLoading, isError } =
+    useGetSomeItemForHomeQuery(filterAndPagination);
+  // add to card function
+  const handlAddToCart = (e) => {
+    if (custID) {
+      addToCart({
+        product: e.target.id,
+        customer: custID,
+      });
+    }
+  };
   function readMore(text) {
     if (window.innerWidth >= 1200) {
       return text.slice(0, 50);
@@ -68,13 +59,30 @@ function ItemPage() {
       return text.slice(0, 150);
     }
   }
+  const handleChnageData = (val) => {
+    setFilterAndPagination({
+      ...filterAndPagination,
+      page: val,
+    });
+  };
   function discountPercent(value) {
     value = String(100 - value * 100);
     return value.slice(0, 2);
   }
+  const handleGetSearchData = (e) => {
+    setFilterAndPagination({
+      ...filterAndPagination,
+      search: e.target.value,
+    });
+  };
+
   return (
     <div className={`max-w-[1500px] m-auto`}>
-      <Navbar showFilterButton="sm:hidden" />
+      <Navbar
+        showSearchbar={true}
+        searchHandl={handleGetSearchData}
+        showFilterButton="sm:hidden"
+      />
       <section
         className={`flex flex-row justify-evenly items-start gap-3 mt-4`}
       >
@@ -93,11 +101,11 @@ function ItemPage() {
           ) : isLoading ? (
             <Loding />
           ) : (
-            data.map((value) => {
-              if (!categorey) {
+            data?.data.map((value) => {
+              {
                 return (
-                  <Link
-                    key={value.id}
+                  <div
+                    key={value._id}
                     className={`flex flex-col h-[350px] w-full sm:w-[45%] md:w-[45%] lg:w-[30%]
                     xl:w-[23%]
                     rounded-xl border relative overflow-hidden`}
@@ -105,7 +113,7 @@ function ItemPage() {
                     {/* imgae ----------------------- image */}
                     <div className={`w-[100%]  border-b relative`}>
                       <img
-                        src={value.image}
+                        src={`http://localhost:4000/uploads/image/${value.image}`}
                         className={`w-full h-[180px] object-cover`}
                       />
                       {value.discount > 0 ? (
@@ -141,18 +149,20 @@ function ItemPage() {
                         )}
                       </div>
                       <h1 className={`text-sm px-2 py-1`}>
-                        {readMore(value.desc)} ....
+                        {readMore(value.description)} ....
                       </h1>
                       <div className="flex items-center absolute bottom-0 left-0 w-full">
-                        <Li_nk
-                          key={value.id}
-                          cssStyle={`w-[50%] text-center border py-1`}
-                          url={`/singleitem/${value.id}`}
-                          content={"Show"}
-                        />
+                        <Link
+                          key={value._id}
+                          className={`w-[50%] text-center border py-1`}
+                          to={`/singleitem/${value._id}`}
+                        >
+                          show
+                        </Link>
                         <button
-                          onClick={() => {
-                            addCardHandle(value.id);
+                          id={value._id}
+                          onClick={(e) => {
+                            handlAddToCart(e);
                           }}
                           className={`w-[50%] border border-jaguar-500 text-white bg-jaguar-500 py-1`}
                         >
@@ -160,73 +170,7 @@ function ItemPage() {
                         </button>
                       </div>
                     </div>
-                  </Link>
-                );
-              } else if (value.category == categorey) {
-                return (
-                  <article
-                    key={value.id}
-                    className={`overflow-hidden rounded-xl w-[90%] sm:w-[45%] lg:w-[30%] xl:w-[23%]
-              shadow-xl h-[250px] sm:h-[350px] flex flex-row sm:flex-col sm:items-center relative  mt-4
-               hover:mt-[10px] hover:shadow-2xl ease-out duration-300`}
-                  >
-                    <div
-                      className={`w-[50%] sm:w-[90%] mt-2 h-[200px] sm:h-[180px] relative`}
-                    >
-                      <img
-                        className={`w-full h-full object-cover`}
-                        src={value.image[0]}
-                      />
-                      <h1
-                        className="absolute bg-blue-900 text-cus-white-50 px-2
-                    top-1 right-1 sm:bottom-1 sm:top-auto sm:left-1 sm:right-auto py-1 rounded-lg "
-                      >
-                        {value.discount > 0 ? (
-                          <span className={`flex gap-1`}>
-                            <span className="line-through text-azure-radiance-100">
-                              ${value.price}
-                            </span>
-                            <span className={`font-bold`}>
-                              %{discountPercent(value.discount / value.price)}
-                            </span>
-                            <span className="font-medium">
-                              ${value.discount}
-                            </span>
-                          </span>
-                        ) : (
-                          <span className="">${value.price}</span>
-                        )}
-                      </h1>
-                    </div>
-                    {/* ---------------- deatails------------------ */}
-                    <div className={` w-[48%] sm:w-[90%] h-fit mt-0 p-1 ml-1`}>
-                      <h1 className={`font-bold text-azure-radiance-950`}>
-                        {value.name}
-                      </h1>
-                      <p
-                        className={`text-sm  overflow-hidden mt-2
-                      text-slate-700 `}
-                      >
-                        {readMore(value.discription)}. . . . .
-                      </p>
-                      <div
-                        className={`flex justify-start text-yellow-400 mt-2`}
-                      >
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star text-azure-radiance-950"></i>
-                        <i className="fa-solid fa-star text-azure-radiance-950"></i>
-                      </div>
-                    </div>
-                    <Link
-                      to={`/singleitem/${value.id}`}
-                      className={`absolute left-0 bottom-0 text-center w-[100%]
-                 bg-azure-radiance-900 py-1 font-semibold text-lg capitalize text-white`}
-                    >
-                      show
-                    </Link>
-                  </article>
+                  </div>
                 );
               }
             })
@@ -234,6 +178,11 @@ function ItemPage() {
         </section>
       </section>
 
+      <Pagination
+        lenght={data?.results}
+        pagination={handleChnageData}
+        pageIndex={filterAndPagination.page}
+      />
       <Footer />
     </div>
   );
