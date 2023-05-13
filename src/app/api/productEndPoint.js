374fc19b39e5a0d01c72a84e0611dbaef934f31e
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-13 17:39:44 +0300

    add some queries to (upload file , retrive order , add product for cart ,retrive products for single page)

--- src/app/api/productEndPoint.js
+++ src/app/api/productEndPoint.js
@@ -2,16 +2,80 @@ import { api } from "./api";
 
 const product = api.injectEndpoints({
   endpoints: (builder) => ({
-    getItems: builder.query({
-      query: () => `products/`,
-      providesTags: ["non_refresh"],
+    getProductOfUsers: builder.query({
+      query: (id) => ({
+        url: `products/user/${id}`,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("user_token")}`,
+        },
+        providesTags: ["non_refresh"],
+      }),
+    }),
+    // order item
+    orderItem: builder.mutation({
+      query: (data) => ({
+        url: `/orders/`,
+        method: "PATCH",
+        body: data,
+        invalidatesTags: ["non_refresh"],
+      }),
     }),
     // retrive some item for home page
     getSomeItemForHome: builder.query({
-      query: () => `api/products/`,
+      query: (data) =>
+        `/products?page=${data.page && data.page}&limit=${
+          data.limit && data.limit
+        }&search=${data.search && data.search}&category=${
+          data.category && data.category
+        }`,
       providesTags: ["non_refresh"],
+    }),
+    // retrive one item for single page
+    getOneItemSinglePage: builder.query({
+      query: (id) => `/products/${id}`,
+      providesTags: ["non_refresh"],
+    }),
+    // add product
+    addProduct: builder.mutation({
+      query: (data) => ({
+        url: `/products/`,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("user_token")}`,
+        },
+        method: "POST",
+        body: data,
+        invalidatesTags: ["non_refresh"],
+      }),
+    }),
+    retriveOrder: builder.query({
+      query: () => `/orders/`,
+      providesTags: ["non_refresh"],
+    }),
+    //upload image
+    uploadImage: builder.mutation({
+      query: (file) => {
+        const formData = new FormData();
+        formData.append("photo", file[0]);
+        return {
+          url: `/products/upload`,
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("user_token")}`,
+          },
+          method: "POST",
+          body: formData,
+          invalidatesTags: ["non_refresh"],
+        };
+      },
     }),
   }),
 });
 
-export const { useGetItemsQuery, useGetSomeItemForHomeQuery } = product;
+export const {
+  useGetProductOfUsersQuery,
+  useGetSomeItemForHomeQuery,
+  useGetOneItemSinglePageQuery,
+  useOrderItemMutation,
+  useRetriveOrderQuery,
+  useAddProductMutation,
+  useUploadImageMutation,
+} = product;
