3d92a9f53028dfa0ebedd55110054225d2e245b0
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-13 21:32:41 +0300

    edite navbar hide search bar show only prodcut page and change

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import cart from "../assets/images/wicker (2).png";
 import cartDark from "../assets/images/wicker.png";
@@ -6,9 +6,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { dark, light } from "../Slice/themeSlice";
 import "./components.css";
 import Li_nk from "./Li_nk";
-function Test() {
+function MainNavbar({ searchHandl, showSearchbar }) {
   const isDark = useSelector((state) => state.theme.value);
-  const showSginUp = useSelector((state) => state.user.value);
+  const showSginUp = useSelector((state) => state.user.user);
+  const [searchValue, setSearchValue] = useState("");
+  // console.log(data?.data);
   const [isOpen, setISOpen] = useState(false);
 
   const dispatch = useDispatch();
@@ -30,34 +32,41 @@ function Test() {
     myDivRef.current.scrollLeft -= 50;
   }
   return (
-    <nav className={`mb-[50px] ${isDark ? `bg-gray-800` : ``}`}>
+    <nav className={`mb-[20px] border-b ${isDark ? `bg-gray-800` : ``}`}>
       <div className={`flex justify-center items-center p-2 gap-x-2 relative`}>
         <div
           className={`flex items-center gap-x-2 overflow-hidden w-[90%] lg:w-[40%]`}
         >
-          <Li_nk
-            url="/"
-            cssStyle={`text-[2rem] font-semibold pb-[2px] font-serif ${
-              isDark ? textColor : ` text-red-300`
+          <Link
+            to="/"
+            className={`text-[2rem] font-semibold pb-[2px] font-serif ${
+              isDark ? textColor : ` text-jaguar-400`
             } `}
-            content={"Bikra"}
-          />
-          <div
-            className={`border-[2px] overflow-hidden rounded-[2rem] flex
+          >
+            Bikra
+          </Link>
+          {showSearchbar ? (
+            <div
+              className={`border-[2px] overflow-hidden rounded-[2rem] flex
           justify-center items-center   w-[100%] h-[2.5rem] lg:h-[3rem]
           `}
-          >
-            <input
-              type="search"
-              className={`h-full outline-none w-full p-[2px] px-3 bg-transparent`}
-              placeholder="Search Bikra"
-            />
-            <button
-              className={`h-full px-4 hover:bg-jaguar-200 ease-in-out duration-300`}
             >
-              <i className="fa-solid fa-magnifying-glass fa-lg"></i>
-            </button>
-          </div>
+              <input
+                type="search"
+                onKeyUp={(e) => searchHandl(e)}
+                className={`h-full outline-none w-full p-[2px] px-3 bg-transparent`}
+                placeholder="Search Bikra"
+              />
+
+              <button
+                className={`h-full px-4 hover:bg-jaguar-200 ease-in-out duration-300`}
+              >
+                <i className="fa-solid fa-magnifying-glass fa-lg"></i>
+              </button>
+            </div>
+          ) : (
+            ``
+          )}
         </div>
         <div
           className={`flex absolute flex-col top-0 ${
@@ -79,7 +88,7 @@ function Test() {
           >
             <i className="fa-regular fa-circle-xmark fa-2xl "></i>
           </button>
-          {showSginUp ? (
+          {showSginUp?.email ? (
             ``
           ) : (
             <h1
@@ -90,9 +99,10 @@ function Test() {
              isDark ? `bg-jaguar-400  text-white` : `bg-jaguar-400  text-white`
            }`}
             >
-              <Link to={`/signup`}>Create a Shop</Link>
+              <Link to={`/signup`}>sign up</Link>
             </h1>
           )}
+
           <h1
             className={`rounded-[3rem] border-[2px]  h-[3rem] overflow-hidden items-center flex px-4
            text-black font-semibold ease-in-out duration-300
@@ -102,16 +112,26 @@ function Test() {
                  : ` hover:bg-jaguar-100`
              }`}
           >
-            <Link to={`/login`}>{showSginUp ? `My Shop` : `Login`}</Link>
+            <Link to={showSginUp?.email ? `/account` : `/login`}>
+              {showSginUp?.email ? `My Shop` : `Login`}
+            </Link>
           </h1>
-          <h1
-            className={`rounded-[3rem] h-[3rem] overflow-hidden items-center flex px-4
-           text-black font-semibold  ease-in-out duration-300
-            ${isDark ? `text-white` : `hover:bg-jaguar-100`}`}
-          >
-            <Link to={`/`}>About</Link>
-          </h1>
-          <Link
+
+          {showSearchbar ? (
+            ``
+          ) : (
+            <h1
+              className={`rounded-[3rem] h-[3rem] border-[2px]  
+             overflow-hidden items-center flex px-4 mt-4 lg:mt-0
+           text-oasis-950 font-semibold hover:bg-jaguar-500 ease-in-out duration-300
+           ${
+             isDark ? `bg-jaguar-400  text-white` : `bg-jaguar-400  text-white`
+           }`}
+            >
+              <Link to={`/item`}>Products</Link>
+            </h1>
+          )}
+          {/* <Link
             to={`/`}
             className={`h-[3rem] w-[3rem] flex items-center justify-center rounded-full 
            ease-in-out duration-300  ${
@@ -119,21 +139,22 @@ function Test() {
            }`}
           >
             <i className={`fa-regular fa-heart fa-xl `}></i>
-          </Link>
-          <Li_nk
-            url={`/cart`}
-            cssStyle={`h-[3rem] w-[3rem] flex items-center justify-center rounded-full
+          </Link> */}
+
+          <Link
+            to={`/cart`}
+            className={`h-[3rem] w-[3rem] flex items-center justify-center rounded-full
            ease-in-out duration-300 pb-[2px] ${
              isDark ? `hover:bg-jaguar-400` : `hover:bg-jaguar-100  `
            }`}
-            content={
+          >
+            {
               <img
                 src={isDark ? cartDark : cart}
                 className={`w-[55%] h-[55%]`}
               />
             }
-            isScrollTop={true}
-          />
+          </Link>
           <button
             onClick={() => {
               handlClick();
@@ -171,4 +192,4 @@ function Test() {
   );
 }
 
-export default Test;
+export default MainNavbar;
