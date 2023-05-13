122935fee583943fff950e56c5472f3a97dedcd5
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-13 17:17:33 +0300

    change base url

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
+    // baseUrl: "https://bikra-backend.onrender.com/api/",
+    baseUrl: "http://localhost:4000/api/",
   }),
   tagTypes: ["non_refresh"],
   endpoints: () => ({}),
