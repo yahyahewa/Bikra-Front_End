44e0581343f8846f2151e422301397b2017380d5
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-13 19:27:52 +0300

    connect with server and retrive data from server

--- src/components/CategorySlide.jsx
+++ src/components/CategorySlide.jsx
@@ -1,39 +1,19 @@
 import React from "react";
 import "./components.css";
 import { Link } from "react-router-dom";
+import { useGetCategoreyQuery } from "../app/api/categorey.endpoint";
 function CategorySlide() {
-  const category = [
-    {
-      id: 1,
-      name: "Fashion",
-      image:
-        "https://as1.ftcdn.net/v2/jpg/02/66/11/50/1000_F_266115052_d3SVcKWO6LWrObPskrjpaM7tucWfF0vj.jpg",
-    },
-    {
-      id: 2,
-      name: "Beauty",
-      image:
-        "https://akns-images.eonline.com/eol_images/Entire_Site/2022122/rs_1024x759-220222125215-1024-e-comm-Nordstrom-Beauty-Bestsellers.jpg?fit=around%7C1024:759&output-quality=90&crop=1024:759;center,top",
-    },
-    {
-      id: 3,
-      name: "Electronics",
-      image:
-        "https://www.siliconindia.com/news/newsimages/Webp.net-resizeimage%20(18).jpg",
-    },
-    {
-      id: 4,
-      name: "Home",
-      image:
-        "https://simg.nicepng.com/png/small/423-4234218_trend-features-a-collection-of-painted-vintage-furniture.png",
-    },
-    {
-      id: 5,
-      name: "Eksswarat",
-      image:
-        "https://eksswarat.yolasite.com/resources/10508303_1447955725463232_611729770_n.jpg",
-    },
-  ];
+  const category = [];
+  if (true) {
+    const { data, isError, isLoading } = useGetCategoreyQuery();
+    if (!isLoading) {
+      if (data) {
+        data.data.forEach((value) => {
+          category.push(value);
+        });
+      }
+    }
+  }
   return (
     <div className="mt-10 ">
       <section
@@ -48,8 +28,8 @@ function CategorySlide() {
         {category.map((value) => {
           return (
             <Link
-              to={`/item/${value.id}`}
-              key={value.id}
+              to={`/item/${value._id}`}
+              key={value._id}
               className={` inline-block mt-7 mx-3 md:mx-5
               rounded-xl shadow-xl overflow-hidden 
               mb-3 bg-cus-white-50 w-[120px] h-[190px] sm:w-[140px] sm:h-[210px]
@@ -64,7 +44,7 @@ function CategorySlide() {
                 <img
                   className={` rounded w-[120px] h-[140px] sm:w-[140px] sm:h-[150px]
                   md:w-[190px] lg:w-[250px]  object-cover`}
-                  src={value.image}
+                  src={`http://localhost:4000/uploads/image/${value.image}`}
                 />
                 <h1
                   className={` text-xs ml-1 md:text-base underline text-azure-radiance-900 font-medium`}
