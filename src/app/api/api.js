d78beb3bb94fad061eb8399ac14b8bea106f6de8
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-06-06 21:52:46 +0300

    a

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://bikra-backend.onrender.com/api/",
-    baseUrl: "http://localhost:4000/api/",
+    baseUrl: import.meta.env.VITE_BACK_END,
+    // baseUrl: "http://localhost:4000/api/",
   }),
   tagTypes: ["non_refresh"],
   endpoints: () => ({}),
